516f2d8918c777195e09eff34234d179c90f690d
Onuraslann <theeaslann1@gmail.com>
2021-10-29 23:42:02 +0300

    Autofac commit

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<DepartmanManager>().As<IDepartmanService>().SingleInstance();
+            builder.RegisterType<EfDepartmanDal>().As<IDepartmanDal>().SingleInstance();
+
+            builder.RegisterType<LecturerManager>().As<ILecturerService>().SingleInstance();
+            builder.RegisterType<EfLecturerDal>().As<ILecturerDal>().SingleInstance();
+
+            builder.RegisterType<LessonManager>().As<ILessonService>().SingleInstance();
+            builder.RegisterType<EfLessonDal>().As<ILessonDal>().SingleInstance();
+
+            builder.RegisterType<NoteManager>().As<INoteService>().SingleInstance();
+            builder.RegisterType<EfNoteDal>().As<INoteDal>().SingleInstance();
+
+            builder.RegisterType<StudentManager>().As<IStudentService>().SingleInstance();
+            builder.RegisterType<EfStudentDal>().As<IStudentDal>().SingleInstance();
+        }
+    }
+}
