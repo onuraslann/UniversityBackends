8bcf16832ca2ef397b91db27fa61cf89014b45aa
Onuraslann <theeaslann1@gmail.com>
2021-10-29 21:33:51 +0300

    Dtos Commit

--- DataAccess/Concrete/EntityFramework/EfLessonDal.cs
+++ DataAccess/Concrete/EntityFramework/EfLessonDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,31 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfLessonDal: EntityRepositoryBase<Lesson,UniversityContext>,ILessonDal
+    public class EfLessonDal : EntityRepositoryBase<Lesson, UniversityContext>, ILessonDal
     {
+        public List<LessonDtoDetails> GetByDtos()
+        {
+            using (UniversityContext context = new UniversityContext())
+            {
+                var result = from lecturer in context.Lecturers
+                             join lesson in context.Lessons
+                         on lecturer.Id equals lesson.LecturerId
+                             join student in context.Students
+                             on lesson.StudentId equals student.Id
+                             select new LessonDtoDetails
+                             {
+
+                                 LessonName = lesson.LessonName,
+                                 Credit = lesson.Credit,
+                                 LecturerEmail = lecturer.Email,
+                                 LecturerFirstName = lecturer.FirstName,
+                                 StudentFirstName = student.FirstName,
+                                 StudentNo = student.StudentNo
+
+
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
---
+++ Entities/DTOs/LessonDtoDetails.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class LessonDtoDetails: IDto
+    {
+        public string LessonName { get; set; }
+        public int Credit { get; set; }
+        public string StudentNo { get; set; }
+        public string StudentFirstName { get; set; }
+        public string LecturerFirstName { get; set; }
+        public string LecturerEmail { get; set; }
+    }
+}
